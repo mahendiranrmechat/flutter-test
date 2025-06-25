de15acfa1f4586732a0b841b698f56e5b8fa30d9
mahendiranrmechat <130583491+mahendiranrmechat@users.noreply.github.com>
2025-06-25 17:34:27 +0530

    Delete HansWehrDictionary-master directory

--- HansWehrDictionary-master/lib/service_locator.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get_it/get_it.dart';
-import 'services/local_storage_service.dart';
-
-GetIt locator = GetIt.instance;
-
-Future setupLocator() async {
-  var instance = await LocalStorageService.getInstance();
-  locator.registerSingleton<LocalStorageService>(instance!);
-}
